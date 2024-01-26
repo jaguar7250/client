d30f3aa8bc4f11ef1f8076388e13008ccb14f8b0
jaguar7250 <114855508+jaguar7250@users.noreply.github.com>
2024-01-26 13:03:15 +0530

    Add files via upload

---
+++ routes/authRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import {registerController,loginController, testController, forgotPasswordController} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js"; 
+
+
+const router=express.Router();
+
+router.post("/register",registerController);
+router.post("/login",loginController);
+router.get("/test",requireSignIn,isAdmin,testController);
+router.get("/user-auth",requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true});
+})
+router.post('/forgot-password',forgotPasswordController);
+export default router;
